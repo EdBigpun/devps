ce208a8735da2de595e2147a55c5cf7b6668f956
Jjfonsecab <119600965+Jjfonsecab@users.noreply.github.com>
2024-07-23 07:15:49 -0500

    Actualizando Cors

--- api/apiw/src/main/java/com/wheelz/api/ApiApplication.java
+++ api/apiw/src/main/java/com/wheelz/api/ApiApplication.java
@@ -4,6 +4,9 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
@@ -11,6 +14,16 @@ public class ApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**").allowedHeaders("*").allowedMethods("*");
+            }
+        };
     }
 
 }
